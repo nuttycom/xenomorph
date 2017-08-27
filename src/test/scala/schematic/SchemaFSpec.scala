2a11a73ba5c059f4124805d5d745b9dbb782c14d
Kris Nuttycombe <kris.nuttycombe@gmail.com>
2017-08-27 16:12:16 -0600

    License schematic under LGPLV3

--- src/test/scala/schematic/SchemaFSpec.scala
+++ src/test/scala/schematic/SchemaFSpec.scala
@@ -1,3 +1,17 @@
+/*
+ * Copyright (C) 2017 Kris Nuttycombe
+ * All rights reserved.
+ *
+ * This file is part of the Scala Schematic library.
+ *
+ * GNU Lesser General Public License Usage
+ * This file may be used under the terms of the GNU Lesser
+ * General Public License version 3.0 as published by the Free Software
+ * Foundation and appearing in the file LICENSE included in the
+ * packaging of this file.  Please review the following information to
+ * ensure the GNU Lesser General Public License version 3.0 requirements
+ * will be met: https://www.gnu.org/licenses/lgpl-3.0.txt
+ */
 package schematic
 
 import scalaz.syntax.apply._
